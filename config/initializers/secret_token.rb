e60dd494a60e56029c88ef8f01331aad6e4c1e2f
Kevin <apprentice@master-inf-4.local>
2014-06-28 13:08:56 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Askme::Application.config.secret_token = '935a0d777d4a5db3db3ddb1d66aed6a185ab2511986db03e6b3d8fed8436414169116b4cff3cf1f3fb20dbd93ff1d3a2dcf89faae9a3636c27b96a362f0c3ac4'
