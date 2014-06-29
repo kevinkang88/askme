d33549b27c9137bab365b883caab5417670c699b
Kevin Kang & rubberyuzu <pair+kevinkang88.rubberyuzu@devbootcamp.com>
2014-06-28 19:57:33 -0700

    finished sessions with stephen

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,25 @@
 class PagesController < ApplicationController
+
+  before_filter :check_session, except: [:about]
+  # this is calling check_session method for all actions except :about
+
+  def about
+    # this is a public page for everyone
+    # session is not checked here
+  end
+
   def main
+    # params
+    # request
+    binding.pry
 
+    @user = User.new
   end
-  def create
 
+  def test
+    # this is a private page for logged in users
   end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Askme::Application.routes.draw do
   root to: 'pages#main'
   resources :users
+  resources :sessions, only: [:destroy, :create]
+  get '/test', to: 'pages#test', as: 'test'
 end
